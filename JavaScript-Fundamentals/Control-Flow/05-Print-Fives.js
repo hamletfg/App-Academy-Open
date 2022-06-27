55077305aec2339dadbf530e067ee14d5f47fe97
hamletfg <61448877+hamletfg@users.noreply.github.com>
2022-06-27 14:17:45 -0600

    Update 05-Print-Fives.js

--- JavaScript-Fundamentals/Control-Flow/05-Print-Fives.js
+++ JavaScript-Fundamentals/Control-Flow/05-Print-Fives.js
@@ -1,3 +1,4 @@
+// FOR LOOP
 function printFives1For(max) { // create a function and call the paramenter "max"
   for (let i = 0; i < max; i += 1) { // create a for loop that has the variable i = 0, "i" cannot be greater than input (max), add 1 for each loop
     if (i % 5 === 0) { // this is how we make sure that the number can be divided by 5 with no remainder
@@ -7,3 +8,17 @@ function printFives1For(max) { // create a function and call the paramenter "max
 }
 
 printFives1For(29);
+
+
+// WHILE LOOP
+function printFives1(max) {
+  let i = 0;
+  while (i < max) {
+    if (i % 5 === 0) {
+      console.log(i);
+    }
+    i++;
+  }
+}
+
+printFives1(19)
