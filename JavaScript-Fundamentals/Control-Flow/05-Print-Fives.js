dd00e8de4e46498df7d2c039bec0170e5bf760b2
hamletfg <61448877+hamletfg@users.noreply.github.com>
2022-06-27 20:36:17 -0600

    Update 05-Print-Fives.js

--- JavaScript-Fundamentals/Control-Flow/05-Print-Fives.js
+++ JavaScript-Fundamentals/Control-Flow/05-Print-Fives.js
@@ -1,4 +1,6 @@
-// FOR LOOP
+// From research that I've done it seems like for loops are more recommended than while loops
+
+// FUNCTION 1
 function printFives1For(max) { // create a function and call the paramenter "max"
   for (let i = 0; i < max; i += 1) { // create a for loop that has the variable i = 0, "i" cannot be greater than input (max), add 1 for each loop
     if (i % 5 === 0) { // this is how we make sure that the number can be divided by 5 with no remainder
@@ -7,10 +9,9 @@ function printFives1For(max) { // create a function and call the paramenter "max
   }
 }
 
-printFives1For(29);
+printFives1For(19)
 
-
-// WHILE LOOP
+// FUNCTION 2
 function printFives1(max) {
   let i = 0;
   while (i < max) {
@@ -22,3 +23,25 @@ function printFives1(max) {
 }
 
 printFives1(19)
+
+// FUNCTION 3
+function printFives1(max) {
+  let i = 0;
+  while (i < max) {
+    if (i % 5 === 0) {
+      console.log(i);
+    }
+    i++;
+  }
+}
+
+// FUNCTION 4
+function printFives2While(max) {
+  let i = 0;
+  while (i < max) {
+    console.log(i);
+    i+= 5;
+  }
+}
+
+printFives2While(19)
