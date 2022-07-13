48dacf0713b1f48c18835bacc69d3f1c74cda6b4
hamletfg <61448877+hamletfg@users.noreply.github.com>
2022-07-13 11:14:26 -0600

    Update 05-Log-Between-Stepper.js

--- JavaScript-Fundamentals/Control-Flow/05-Log-Between-Stepper.js
+++ JavaScript-Fundamentals/Control-Flow/05-Log-Between-Stepper.js
@@ -1,6 +1,8 @@
-function logBetweenStepperFor(min, max, step) {
-  for (let i = min; i<= max; i += step) {
-    console.log(i);
+// Assignment: Create a function that takes in 3 numbers as parameters. The function should print out numbers between [min] and [max] at [step] intervals.
+
+function logBetweenStepperFor(min, max, step) {   // Create the function, have it use a for loop and name it logBeweenStepperFor
+  for (let i = min; i<= max; i += step) {         // For loop, let "i" equal min, then compare it to max and make sure it's less than or equal to,
+    console.log(i);                               // add a "step" taken from the inputed value to add to it, repeat until max value is met.
   }
 }
 
